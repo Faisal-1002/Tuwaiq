f876a674fd8d01e6ce69387d5ec6ef782298053d
Faisal Alghamdi <85634078+Faisal-1002@users.noreply.github.com>
2025-02-21 21:17:10 +0300

    Add files via upload

--- Project01/tic-tac-toe/Main.java
+++ Project01/tic-tac-toe/Main.java
@@ -4,277 +4,138 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner data = new Scanner(System.in);
-
-        System.out.println("Welcome to tic tac toe game, please choose to play with 'x' or 'o' : ");
-        String playerSign = data.nextLine();
-        String computerSign = "";
-        int turns = 0;
-        if(playerSign.equalsIgnoreCase("x"))
-            computerSign = "o";
+        System.out.println("Welcome to Tic-Tac-Toe! Choose 'X' or 'O': ");
+        String playerSign = data.next().toUpperCase();
+        String computerSign;
+        
+        if (playerSign.equals("X"))
+            computerSign = "O";
         else
-            computerSign = "x";
+            computerSign = "X";
 
-        System.out.println("You want to play 1 round or 3 rounds");
-        int choice = data.nextInt();
-        if(choice == 3){
-            System.out.println("Since you chose 3 if you or the computer got 2 wins game will end");
-        }
+        System.out.println("How many rounds do you want to play?");
+        int rounds = data.nextInt();
+        int playerWins = 0, computerWins = 0, ties = 0;
 
-        int playerWins = 0;
-        int computerWins = 0;
+        for (int i = 0; i < rounds; i++) {
+            String[][] board = {{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}};
+            int turns = 0;
 
-        while (true){
-            String[][] gameBoard = {{"1","2","3"},{"4","5","6"},{"7","8","9"}};
-            printBoard(gameBoard);
-            int tie = 0;
-            while (true){
-                System.out.println("Enter a valid position from 1-9 to play");
-                int position = data.nextInt();
-                play(gameBoard,position,playerSign);
-                System.out.println("Your move : ");
-                printBoard(gameBoard);
-                if (checkWin(gameBoard,playerSign)){
-                    System.out.println("You won !!");
+            System.out.println("\nRound " + (i + 1) + " begins!");
+            printBoard(board);
+
+            while (true) {
+                // Player Move
+                int position = getPlayerMove(data, board);
+                makeMove(board, position, playerSign);
+                printBoard(board);
+
+                // Check Win Condition
+                if (checkWin(board, playerSign)) {
+                    System.out.println("You won this round!");
                     playerWins++;
                     break;
                 }
                 turns++;
-                if (turns == 9){
-                    System.out.println("Results : Tie");
-                    tie++;
+
+                // Check if tie
+                if (turns == 9) {
+                    System.out.println("It's a tie!");
+                    ties++;
                     break;
                 }
-                computerPlay(gameBoard,computerSign);
-                System.out.println("Computer move : ");
-                printBoard(gameBoard);
-                if (checkWin(gameBoard,computerSign)){
-                    System.out.println("Computer won !!");
+
+                // Computer Move
+                int compPosition = getComputerMove(board);
+                makeMove(board, compPosition, computerSign);
+                System.out.println("\nComputer's move:");
+                printBoard(board);
+
+                // Check Win Condition
+                if (checkWin(board, computerSign)) {
+                    System.out.println("Computer won this round!");
                     computerWins++;
                     break;
                 }
                 turns++;
             }
-            System.out.println("Player wins : " + playerWins);
-            System.out.println("Computer wins : " + computerWins);
-            System.out.println("Tie : " + tie);
-            if(choice == 1){
-                break;
-            } else if (choice == 3 && playerWins == 2) {
-                System.out.println("Final result : You won !!!");
-                break;
-            } else if (choice == 3 && computerWins == 2) {
-                System.out.println("Final result : Unfortunately the computer won");
-                break;
-            }
-        }
-    }
-    public static void printBoard(String[][] gameBoard){
-        System.out.println(gameBoard[0][0] + " | " + gameBoard[0][1] + " | " + gameBoard[0][2]);
-        System.out.println("----------");
-        System.out.println(gameBoard[1][0] + " | " + gameBoard[1][1] + " | " + gameBoard[1][2]);
-        System.out.println("----------");
-        System.out.println(gameBoard[2][0] + " | " + gameBoard[2][1] + " | " + gameBoard[2][2]);
-    }
-    public static String[][] play(String[][] gameBoard, int position, String xOrO){
-        Scanner data = new Scanner(System.in);
-        boolean condtiotn = true;
-        while (condtiotn){
-            switch (position){
-                case 1:
-                    if(gameBoard[0][0].equalsIgnoreCase("x") || gameBoard[0][0].equalsIgnoreCase("o")){
-                        System.out.println("Positioned occupied, try again");
-                        break;                }
-                    else {
-                        gameBoard[0][0] = xOrO;
-                        condtiotn = false;
 
-                        break;                }
-                case 2:
-                    if(gameBoard[0][1].equalsIgnoreCase("x") || gameBoard[0][1].equalsIgnoreCase("o")){
-                        System.out.println("Positioned occupied, try again");
-                        break;                }
-                    else {
-                        gameBoard[0][1] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 3:
-                    if(gameBoard[0][2].equalsIgnoreCase("x") || gameBoard[0][2].equalsIgnoreCase("o")){
-                        System.out.println("Positioned occupied, try again");
-                        break;                }
-                    else {
-                        gameBoard[0][2] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 4:
-                    if(gameBoard[1][0].equalsIgnoreCase("x") || gameBoard[1][0].equalsIgnoreCase("o")){
-                        System.out.println("Positioned occupied, try again");
-                        break;                }
-                    else {
-                        gameBoard[1][0] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 5:
-                    if(gameBoard[1][1].equalsIgnoreCase("x") || gameBoard[1][1].equalsIgnoreCase("o")){
-                        System.out.println("Positioned occupied, try again");
-                        break;                }
-                    else {
-                        gameBoard[1][1] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 6:
-                    if(gameBoard[1][2].equalsIgnoreCase("x") || gameBoard[1][2].equalsIgnoreCase("o")){
-                        System.out.println("Positioned occupied, try again");
-                        break;                }
-                    else {
-                        gameBoard[1][2] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 7:
-                    if(gameBoard[2][0].equalsIgnoreCase("x") || gameBoard[2][0].equalsIgnoreCase("o")){
-                        System.out.println("Positioned occupied, try again");
-                        break;                }
-                    else {
-                        gameBoard[2][0] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 8:
-                    if(gameBoard[2][1].equalsIgnoreCase("x") || gameBoard[2][1].equalsIgnoreCase("o")){
-                        System.out.println("Positioned occupied, try again");
-                        break;                }
-                    else {
-                        gameBoard[2][1] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 9:
-                    if(gameBoard[2][2].equalsIgnoreCase("x") || gameBoard[2][2].equalsIgnoreCase("o")){
-                        System.out.println("Positioned occupied, try again");
-                        break;                }
-                    else {
-                        gameBoard[2][2] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                default:
-                    return gameBoard;
-            }
-
-            if (condtiotn){
-                System.out.println("Enter different position: ");
-                position = data.nextInt();
-            }
+            System.out.println("\nCurrent Scores:");
+            System.out.println("Player: " + playerWins + " | Computer: " + computerWins + " | Ties: " + ties);
         }
 
-        return gameBoard;
+        // Final Results
+        System.out.println("\nFinal Results:");
+        if (playerWins > computerWins) {
+            System.out.println("Congratulations! You won the game!");
+        } else if (computerWins > playerWins) {
+            System.out.println("The computer won. Better luck next time!");
+        } else {
+            System.out.println("It's a tie!");
+        }
+
+        data.close();
     }
-    public static String[][] computerPlay(String[][] gameBoard, String xOrO){
+
+    public static void printBoard(String[][] board) {
+        System.out.println("\n     |     |     ");
+        System.out.println("  " + board[0][0] + "  |  " + board[0][1] + "  |  " + board[0][2] + "  ");
+        System.out.println("_____|_____|_____");
+        System.out.println("     |     |     ");
+        System.out.println("  " + board[1][0] + "  |  " + board[1][1] + "  |  " + board[1][2] + "  ");
+        System.out.println("_____|_____|_____");
+        System.out.println("     |     |     ");
+        System.out.println("  " + board[2][0] + "  |  " + board[2][1] + "  |  " + board[2][2] + "  ");
+        System.out.println("     |     |     \n");
+    }
+
+    public static int getPlayerMove(Scanner data, String[][] board) {
+        int position;
+        while (true) {
+            System.out.print("Enter a position (1-9): ");
+            if (!data.hasNextInt()) {
+                data.next(); // Clear invalid input
+                System.out.println("Invalid input. Please enter a number between 1-9.");
+                continue;
+            }
+            position = data.nextInt();
+
+            if (position >= 1 && position <= 9 && isPositionAvailable(board, position)) {
+                return position;
+            }
+            System.out.println("Invalid or occupied position, try again.");
+        }
+    }
+
+    public static int getComputerMove(String[][] board) {
         Random random = new Random();
-        boolean condtiotn = true;
-
-        while (condtiotn){
-            int position = random.nextInt(1,10);
-            switch (position){
-                case 1:
-                    if(gameBoard[0][0].equalsIgnoreCase("x") || gameBoard[0][0].equalsIgnoreCase("o")){
-                        break;                }
-                    else {
-                        gameBoard[0][0] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 2:
-                    if(gameBoard[0][1].equalsIgnoreCase("x") || gameBoard[0][1].equalsIgnoreCase("o")){
-                        break;                }
-                    else {
-                        gameBoard[0][1] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 3:
-                    if(gameBoard[0][2].equalsIgnoreCase("x") || gameBoard[0][2].equalsIgnoreCase("o")){
-                        break;                }
-                    else {
-                        gameBoard[0][2] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 4:
-                    if(gameBoard[1][0].equalsIgnoreCase("x") || gameBoard[1][0].equalsIgnoreCase("o")){
-                        break;                }
-                    else {
-                        gameBoard[1][0] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 5:
-                    if(gameBoard[1][1].equalsIgnoreCase("x") || gameBoard[1][1].equalsIgnoreCase("o")){
-                        break;                }
-                    else {
-                        gameBoard[1][1] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 6:
-                    if(gameBoard[1][2].equalsIgnoreCase("x") || gameBoard[1][2].equalsIgnoreCase("o")){
-                        break;                }
-                    else {
-                        gameBoard[1][2] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 7:
-                    if(gameBoard[2][0].equalsIgnoreCase("x") || gameBoard[2][0].equalsIgnoreCase("o")){
-                        break;                }
-                    else {
-                        gameBoard[2][0] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 8:
-                    if(gameBoard[2][1].equalsIgnoreCase("x") || gameBoard[2][1].equalsIgnoreCase("o")){
-                        break;                }
-                    else {
-                        gameBoard[2][1] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                case 9:
-                    if(gameBoard[2][2].equalsIgnoreCase("x") || gameBoard[2][2].equalsIgnoreCase("o")){
-                        break;                }
-                    else {
-                        gameBoard[2][2] = xOrO;
-                        condtiotn = false;
-
-                        break;                }
-                default:
-                    return gameBoard;
-            }
-        }
-        return gameBoard;
+        int position;
+        do {
+            position = random.nextInt(9) + 1;
+        } while (!isPositionAvailable(board, position));
+        return position;
     }
-    public static boolean checkWin(String[][] gameBoard, String xOrO) {
-        //Check 8 cases for the win status!!
-        if(
-        //Horizontal check
-        gameBoard[0][0].equalsIgnoreCase(xOrO) && gameBoard[0][1].equalsIgnoreCase(xOrO) && gameBoard[0][2].equalsIgnoreCase(xOrO) ||
-        gameBoard[1][0].equalsIgnoreCase(xOrO) && gameBoard[1][1].equalsIgnoreCase(xOrO) && gameBoard[1][2].equalsIgnoreCase(xOrO) ||
-        gameBoard[2][0].equalsIgnoreCase(xOrO) && gameBoard[2][1].equalsIgnoreCase(xOrO) && gameBoard[2][2].equalsIgnoreCase(xOrO) ||
-        //Vertical check
-        gameBoard[0][0].equalsIgnoreCase(xOrO) && gameBoard[1][0].equalsIgnoreCase(xOrO) && gameBoard[2][0].equalsIgnoreCase(xOrO) ||
-        gameBoard[0][1].equalsIgnoreCase(xOrO) && gameBoard[1][1].equalsIgnoreCase(xOrO) && gameBoard[2][1].equalsIgnoreCase(xOrO) ||
-        gameBoard[0][2].equalsIgnoreCase(xOrO) && gameBoard[1][2].equalsIgnoreCase(xOrO) && gameBoard[2][2].equalsIgnoreCase(xOrO) ||
-        //Diagonal check
-        gameBoard[0][0].equalsIgnoreCase(xOrO) && gameBoard[1][1].equalsIgnoreCase(xOrO) && gameBoard[2][2].equalsIgnoreCase(xOrO) ||
-        gameBoard[0][2].equalsIgnoreCase(xOrO) && gameBoard[1][1].equalsIgnoreCase(xOrO) && gameBoard[2][0].equalsIgnoreCase(xOrO)
-        )
-            return true;
-        else
-            return false;
+
+    public static boolean isPositionAvailable(String[][] board, int position) {
+        int row = (position - 1) / 3;
+        int col = (position - 1) % 3;
+        return !board[row][col].equalsIgnoreCase("X") && !board[row][col].equalsIgnoreCase("O");
+    }
+
+    public static void makeMove(String[][] board, int position, String sign) {
+        int row = (position - 1) / 3;
+        int col = (position - 1) % 3;
+        board[row][col] = sign;
+    }
+
+    public static boolean checkWin(String[][] board, String sign) {
+        return (board[0][0].equals(sign) && board[0][1].equals(sign) && board[0][2].equals(sign)) ||
+                (board[1][0].equals(sign) && board[1][1].equals(sign) && board[1][2].equals(sign)) ||
+                (board[2][0].equals(sign) && board[2][1].equals(sign) && board[2][2].equals(sign)) ||
+                (board[0][0].equals(sign) && board[1][0].equals(sign) && board[2][0].equals(sign)) ||
+                (board[0][1].equals(sign) && board[1][1].equals(sign) && board[2][1].equals(sign)) ||
+                (board[0][2].equals(sign) && board[1][2].equals(sign) && board[2][2].equals(sign)) ||
+                (board[0][0].equals(sign) && board[1][1].equals(sign) && board[2][2].equals(sign)) ||
+                (board[0][2].equals(sign) && board[1][1].equals(sign) && board[2][0].equals(sign));
     }
 }
